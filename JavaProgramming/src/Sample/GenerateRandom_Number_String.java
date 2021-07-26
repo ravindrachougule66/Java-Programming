121ddb3d1297c4f1edda37108b07996545cfd69b
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-07-26 12:18:29 +0530

    updated

---
+++ JavaProgramming/src/Sample/GenerateRandom_Number_String.java
@@ -0,0 +1,26 @@
+package Sample;
+
+import java.util.Random;
+
+public class GenerateRandom_Number_String {
+
+	public static void main(String[] args) {
+		//using Random class
+		/*
+		Random rand = new Random();
+		int rand_int = rand.nextInt(1000);
+		System.out.println(rand_int);
+		
+		double rand_double = rand.nextDouble();
+		System.out.println(rand_double);
+		*/
+		//using Math class
+		//System.out.println(Math.random());
+		
+		//Apache common lang API
+		
+		
+
+	}
+
+}
