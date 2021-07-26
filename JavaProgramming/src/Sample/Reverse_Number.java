6ac478bd019662817884f68cf9d84f719f54250a
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-07-26 08:21:55 +0530

    reverse string

--- JavaProgramming/src/Sample/Reverse_Number.java
+++ JavaProgramming/src/Sample/Reverse_Number.java
@@ -1,9 +1,38 @@
 package Sample;
 
+import java.util.Scanner;
+
 public class Reverse_Number {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);		
+		System.out.println("Enter a number: ");
+		
+		int num = sc.nextInt();
+		
+		//using algorithm
+		/*
+		int rev = 0;
+		
+		while(num!=0)
+		{
+			rev= rev*10 + num%10;
+			num = num/10;
+		}*/
+		
+		//string buffer class
+		/*
+		StringBuffer sb = new StringBuffer(String.valueOf(num));
+		StringBuffer rev = sb.reverse();*/
+		
+		//string builder class
+		
+		StringBuilder sbl = new StringBuilder();
+		sbl.append(num);
+		StringBuilder rev = sbl.reverse();
+		
+		System.out.println("Reverse number is : "+rev);
 
 	}
 
