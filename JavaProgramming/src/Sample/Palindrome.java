e46d86baf20b35e9f574953279bb2fd1ac4747d0
ravindrachougule66 <ravindrachougule66@gmail.com>
2021-07-26 09:11:32 +0530

    updated

---
+++ JavaProgramming/src/Sample/Palindrome.java
@@ -0,0 +1,32 @@
+package Sample;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number :");
+		
+		int num = sc.nextInt();
+		int ori_num=num;
+		int rev=0;
+		while(num!=0)
+		{
+			rev = rev*10 +num%10;
+			num=num/10;
+		}
+		
+		if(ori_num==rev)
+		{
+			System.out.println(ori_num+" Number is Palindrome");
+		}
+		else
+		{
+			System.out.println(ori_num+" Number is not Palindrome");
+		}
+
+	}
+
+}
